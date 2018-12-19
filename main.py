a9c1f0cc69ae3ab24eca5df92adbbe41ff9ee925
AgafonovK <konstantinagaf@gmail.com>
2018-12-19 23:57:34 +0500

    add func for db

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import sys
+
 import postgresql
+from PyQt5.QtWidgets import (QTableWidgetItem,
+                             QApplication, QTableWidget, QAction, QMainWindow)
+
 import gui
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,QTableWidgetItem,
-                             QTextEdit, QGridLayout, QApplication, QPushButton, QTableWidget, QAction, QMainWindow)
-from PyQt5.QtCore import QSize,Qt
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.SecondWindow = gui.SecondWindow(self)
         self.initUI()
 
-
     def initUI(self):
-        personTable = QTableWidget(self)
-        self.setCentralWidget(personTable)
+        self.personTable = QTableWidget(self)
+        self.setCentralWidget(self.personTable)
 
-        addPersonAction = QAction('+ Add ',self)
+        addPersonAction = QAction('+ Add ', self)
         addPersonAction.setStatusTip('Add Person')
         addPersonAction.triggered.connect(self.buttonAddClicked)
 
@@ -32,35 +33,26 @@ class MainWindow(QMainWindow):
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(addPersonAction)
         fileMenu.addAction(exitAction)
+        fileMenu = menubar.addMenu('&Setting')
 
         toolbar = self.addToolBar('Exit')
-        toolbar.addAction(exitAction)
         toolbar.addAction(addPersonAction)
+        toolbar.addAction(exitAction)
 
-        with postgresql.open("pq://postgres:postgres@localhost:5432/book") as db:
-            query = db.prepare("select * from user_book")
-            rowCount = len(query())
-            listUser = query()
-            db.close()
+        #listUser = self.getAllUser()
         #print(listUser)
+        #rowCount = len(listUser)
 
-        personTable.setColumnCount(7)
-        personTable.setRowCount(rowCount)
+        self.personTable.setColumnCount(7)
+        #personTable.setRowCount(rowCount)
 
-        personTable.setHorizontalHeaderLabels(["First name","Second name","Patronimyc","Phone", "Department","Email","Birthday"  ])
-        personTable.horizontalHeader
+        self.refreshAllTableUser(self.personTable)
+        self.personTable.setHorizontalHeaderLabels(
+            ["First name", "Second name", "Patronimyc", "Phone", "Department", "Email", "Birthday"])
+        self.personTable.horizontalHeader
+        self.personTable.resizeColumnsToContents()
 
-        for row in range(0,rowCount):
-            for column in  range(0,7):
-                if type(listUser[row][column+1]=="datetime.date"):
-                    widget = str(listUser[row][column+1])
-                    personTable.setItem(row,column,QTableWidgetItem(widget))
-                else:
-                    personTable.setItem(row,column,QTableWidgetItem(listUser[row][column+1]))
-
-        personTable.resizeColumnsToContents()
-
-        self.setFixedSize(550,350)
+        self.setFixedSize(550, 350)
         ##self.setGeometry(300, 300,550,350)
         self.setWindowTitle('Review')
         self.show()
@@ -69,9 +61,30 @@ class MainWindow(QMainWindow):
         self.secondWindow = gui.SecondWindow()
         self.secondWindow.show()
 
+    def getAllUser(self):
+        with postgresql.open("pq://postgres:postgres@localhost:5432/book") as db:
+            query = db.prepare("select * from user_book")
+            #rowCount = len(query())
+            listUser = query()
+            db.close()
+        return listUser
+
+    def refreshAllTableUser(self, personTable):
+
+        listUser = self.getAllUser()
+        rowCount = len(listUser)
+        personTable.setRowCount(rowCount)
+        for row in range(0, rowCount):
+            for column in range(0, 7):
+                if type(listUser[row][column + 1] == "datetime.date"):
+                    widget = str(listUser[row][column + 1])
+                    personTable.setItem(row, column, QTableWidgetItem(widget))
+                else:
+                    personTable.setItem(row, column, QTableWidgetItem(listUser[row][column + 1]))
+
+
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec_())
---
+++ user.py
@@ -0,0 +1,11 @@
+
+class User(object):
+
+    def __init__(self, firstName, secondName, patronymic, phone, department, email, birthday):
+        self.firstName
+        self.secondName
+        self.patronymic
+        self.phone
+        self.department
+        self.email
+        self.birthday
